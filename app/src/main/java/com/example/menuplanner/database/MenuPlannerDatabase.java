cbf6bc33835bd064eaab683b7d6c4ec0ec1bc75e
Jeramy Dichiera <jeramy.dichiera@gmail.com>
2020-04-29 21:59:40 -0400

    Clean up inserting users

--- app/src/main/java/com/example/menuplanner/database/MenuPlannerDatabase.java
+++ app/src/main/java/com/example/menuplanner/database/MenuPlannerDatabase.java
@@ -5,29 +5,35 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.menuplanner.dao.DayDao;
-import com.example.menuplanner.dao.UserDao;
 import com.example.menuplanner.entity.Day;
-import com.example.menuplanner.entity.User;
 
 @Database(
-        entities = {Day.class, User.class},
+        entities = {Day.class},
         version = 1,
         exportSchema = false)
 public abstract class MenuPlannerDatabase extends RoomDatabase {
     private static MenuPlannerDatabase instance;
     public abstract DayDao dayDao();
-    public abstract UserDao userDao();
 
     public static synchronized MenuPlannerDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     MenuPlannerDatabase.class, "menu_planner_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
 
         return instance;
     }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(SupportSQLiteDatabase db) {
+            super.onCreate(db);
+        }
+    };
 }
--- app/src/main/java/com/example/menuplanner/view/LoginActivity.java
+++ app/src/main/java/com/example/menuplanner/view/LoginActivity.java
@@ -2,7 +2,6 @@ package com.example.menuplanner.view;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProviders;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -16,27 +15,23 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.menuplanner.R;
-import com.example.menuplanner.entity.User;
-import com.example.menuplanner.viewmodel.UserViewModel;
-
-import java.util.Calendar;
 
 public class LoginActivity extends AppCompatActivity {
-    UserViewModel userViewModel;
     EditText username;
     EditText password;
+    SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
-
 
         username = findViewById(R.id.input_login_username);
         password = findViewById(R.id.input_login_password);
         Button login = findViewById(R.id.button_login);
-        
+        database = SQLiteDatabase.openOrCreateDatabase(getApplicationContext()
+                .getDatabasePath("menu_planner_database"), null);
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,34 +68,61 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    private void addData() {
-        User user = new User("TestUser", "123");
-        userViewModel.insert(user);
-    }
-
-    private void deleteData() {
-        SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase(getApplicationContext()
-                .getDatabasePath("menu_planner_database"), null);
-        database.execSQL("DELETE FROM days_table");
-        database.execSQL("delete from sqlite_sequence where name = 'days_table'");
-        database.execSQL("DELETE FROM users_table");
-        database.execSQL("delete from sqlite_sequence where name = 'users_table'");
-    }
-
     private void login() {
+        if (isValidCredential()) {
+            Toast.makeText(this,
+                    "Login yeah",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this,
+                    "Login naah",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    private boolean isValidCredential() {
+        boolean isValid;
         SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase(getApplicationContext()
                 .getDatabasePath("menu_planner_database"), null);
         String name = username.getText().toString();
         String pass = password.getText().toString();
-        boolean valid = database.rawQuery("SELECT userName FROM users_table WHERE userName = :name AND userPassword = :pass", null) != null;
+        Cursor cursor = database.query("users_table", new String[]{"userName"},
+                "userName = ? AND userPassword = ?", new String[]{name, pass},
+                null, null, null, null);
 
+        isValid = (cursor != null) && (cursor.getCount() == 1);
 
+        cursor.close();
+        return isValid;
+    }
 
+    private void addData() {
+        addUserData();
+        addDayData();
+    }
 
-//        boolean valid = userViewModel.validateUser(username.getText().toString(), password.getText().toString());
-        Toast.makeText(this,
-                "Login : " + String.valueOf(valid),
-                Toast.LENGTH_SHORT).show();
+    private void deleteData() {
+        deleteUserData();
+        deleteDayData();
+    }
+
+    private void addUserData() {
+        database.execSQL("CREATE TABLE IF NOT EXISTS users_table " +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, userPassword TEXT)");
+        database.execSQL("INSERT INTO 'users_table' (userName, userPassword) VALUES ('TestUser', '123')");
+    }
+
+    private void deleteUserData() {
+        database.execSQL("DELETE FROM users_table");
+        database.execSQL("DELETE FROM sqlite_sequence where name = 'users_table'");
+    }
+
+    private void addDayData() {
+
+    }
+
+    private void deleteDayData() {
+        database.execSQL("DELETE FROM days_table");
+        database.execSQL("DELETE FROM sqlite_sequence where name = 'days_table'");
     }
 }
